460a86f66e40cdf87c4a606bff8d377426933b2f
lucas mori <morilucasaluno@gmail.com>
2024-11-04 03:26:10 -0300

    feat: configure deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todo-sync",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
